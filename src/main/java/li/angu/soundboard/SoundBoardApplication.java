bb6826f37608743ade68082626769e2bc787b2f4
Fin Reinhard <fin.reinhard@icloud.com>
2018-02-02 21:38:44 +0100

    initialize files

---
+++ src/main/java/li/angu/soundboard/SoundBoardApplication.java
@@ -0,0 +1,12 @@
+package li.angu.soundboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundBoardApplication.class, args);
+	}
+}
